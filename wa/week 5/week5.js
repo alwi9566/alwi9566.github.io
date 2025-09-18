02dbcffc95bfd06c7b671d67a70599b0c623b1f8
alwi9566 <alwi9566@colorado.edu>
2025-09-18 15:57:12 -0600

    i added a commet

---
+++ wa/week 5/week5.js
@@ -0,0 +1,22 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu'); 
+
+
+var menu_show = false
+
+function showMenu() {
+    var shown = navMenu.classList.toggle("show");
+    navMenu.classList.toggle("hide");
+
+    if (show) {
+        navToggle.setAttribute("aria-expanded", "true");
+    }
+
+    else {
+        navToggle.setAttribute("aria-expanded", "false");
+
+}
+
+navToggle.addEventListener('click', showMenu);
+
+/*kjasdhflkjahfjharefi*/
