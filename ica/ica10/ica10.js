9d76bd6238ac61659a3d944d1782e2d558cd6a61
alwi9566 <alwi9566@colorado.edu>
2025-10-21 10:45:24 -0600

    asd

---
+++ ica/ica10/ica10.js
@@ -0,0 +1,46 @@
+let triviaBtn = document.querySelector("#js-new-quote").addEventListener('click',newTrivia);
+
+let answerBtn = document.querySelector('#js-tweet').addEventListener('click',newAnswer);
+
+let current = {
+    question: "",
+    answer: ""
+}
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+async function newTrivia() {
+    //console.log('Success')
+
+    try {
+        const responce = await fetch(endpoint);
+        if (!responce.ok) {
+            throw Error(responce.statusText);
+        }
+        const json = await responce.json();
+        //console.log(json); 
+        displayTrivia(json["question"]);
+        current.question = json["question"];
+        current.answer = json["answer"];
+        console.log(current.question);
+        console.log(current.answer);
+    }
+    catch (err) {
+        console.log(err)
+        alert('Failed to get new trivia');
+    }
+}
+
+function displayTrivia(question) {
+    const questionText = document.querySelector("#js-quote-text");
+    questionText.textContent = question;
+    answerText.textContent = "";
+
+}
+
+function newAnswer() {
+    const answerText = document.querySelector('#js-answer-text');
+    answerText.textContent = current.answer;
+}
+
+newTrivia()
